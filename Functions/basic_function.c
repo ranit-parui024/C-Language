6e6d231d0e4df40f4f9a492ccd44f11a5f21110f
Ranit Parui <ranitparui2005@gmail.com>
2025-04-30 10:49:15 +0530

    Function

---
+++ Functions/basic_function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void greet () {
+    printf("Good Morning!!\n How are you?");
+    return ;
+}
+int main () {
+    printf("Enter your msg = ");
+    scanf(" ");
+    
+    greet () ; // function call
+
+    return 0;
+
+}
