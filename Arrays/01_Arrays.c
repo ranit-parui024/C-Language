b61e7f2bd94235fb7982ae9486e2ddd6dc01ec01
Ranit Parui <ranitparui2005@gmail.com>
2025-05-14 23:52:10 +0530

    arrays

--- Arrays/01_Arrays.c
+++ Arrays/01_Arrays.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main () {
-    int arr[5] = {2, 4, 6, 8, 1}; // declaration + initialization
-    // 5 boxes has created
-    arr[3] = 50; // value of index no 3 has updated
-    printf("%d\n", arr[3]);
-
-    for( int i = 0; i <= 4; i++){
-        printf("%d ", arr[i]); // printing all the elements of array
+    int arr[5];
+    for(int i = 0; i <= 4; i++){
+        printf("Element number %d = ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<=4; i++){
+        printf("%d ", arr[i]);
     }
     return 0;
 }
