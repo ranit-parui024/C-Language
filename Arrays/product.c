b0230f944a8f7ca128b4f03ba3202ba1fb4eefa2
Ranit Parui <ranitparui2005@gmail.com>
2025-05-15 12:33:35 +0530

    product

--- Arrays/product.c
+++ Arrays/product.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+
 int main () {
+    
     int arr[4] = {2, 5, 6, 7}, product = 1;
+
+
     for(int i = 0; i <= 3; i++){
         product = product * arr[i];
     }
+    
     printf("product of the elements is : %d ", product );
-    return 0;
+    
+return 0;
 }
